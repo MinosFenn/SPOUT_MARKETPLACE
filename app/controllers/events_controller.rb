20fbea7eda1b07a80c0177eb24c27f823f5b4f4e
MinosFenn <joachim.simon.45@gmail.com>
2019-05-28 17:57:09 +0200

    eventcreaationpage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
   end
 
@@ -6,14 +8,29 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.organizer = current_user
+
+    if @event.save
+      redirect_to event_path(@event.id)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :activity, :address, :name_of_place, :number_of_places, :date, :time, :picture)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email:"toto@gmail.com", password:"01234567")
+User.create!(email:"CARORORORO@gmail.com", password:"brabrabrab")
+User.create!(email:"CECILECILCIL@gmail.com", password:"dsddsokdskl")
+User.create!(email:"SAYANDADY.com", password:"0hdhdhkdkhd")
+
+
+Event.create!(title:"YOGA", description:"niveau expert", activity:"yoga", address:"26eme centenaire", number_of_places: 20)
+
+Event.create!(title:"SOCCER", description:"270 kilometres", activity:"footing", address:"26eme centenaire", number_of_places: 20)
+
+Event.create!(title:"Course à pieds!", description:"petit foot trql", activity:"foot", address:"26eme centenaire", number_of_places: 15)
+
+Event.create!(title:"Skate", description:"chill n ride", activity:"skateboarding", address:"Friche bel de mai", number_of_places: 10)
