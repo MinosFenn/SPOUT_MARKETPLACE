39c6821969b58242a77e4663800e85d534956546
cecile malbos <malboscecile@gmail.com>
2019-05-28 17:08:47 +0200

    views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,23 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find params[:id]
   end
 
   def new
+    @events = Event.new
   end
 
   def create
+    @events = Event.new(event_params)
+    if @events.save
+      redirect_to event_path(@events.id)
+    else
+      render :new
+    end
   end
 
   def edit
