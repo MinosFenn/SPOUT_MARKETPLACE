4468aad793ccff84eb01f53042a090a5bd39b1e7
cecile malbos <malboscecile@gmail.com>
2019-05-28 17:37:15 +0200

    views show and index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,16 +8,11 @@ class EventsController < ApplicationController
   end
 
   def new
-    @events = Event.new
+
   end
 
   def create
-    @events = Event.new(event_params)
-    if @events.save
-      redirect_to event_path(@events.id)
-    else
-      render :new
-    end
+
   end
 
   def edit
