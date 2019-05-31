62594e6e2f0161c8c49eb8baf115aeec15ef0aeb
carolineplin <caroline.plin1@gmail.com>
2019-05-31 14:53:17 +0200

    validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,11 @@ class Event < ApplicationRecord
   has_many :participants, through: :bookings
   mount_uploader :picture, PhotoUploader
 
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :name_of_place, presence: true
+  validates :address, presence: true
+  validates :number_of_place, presence: true
+  validates :time, presence: true
+  validates :date, presence: true
 end
